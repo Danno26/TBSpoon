e086d77dace4f5f9044f9ece01785d2fe198cf3f
Danno26 <55775106+Danno26@users.noreply.github.com>
2022-01-29 15:22:56 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,37 @@
+/* Responsive NavBar */
+
+let header = document.querySelector('header');
+let menu = document.querySelector('menu-icon');
+let navbar = document.querySelector('navbar');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('active', window.scrollY > 0);
+});
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+/* Carousel  */
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("data-active")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
